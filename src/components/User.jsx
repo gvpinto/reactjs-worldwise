9ae203caaaefc2b2bbfa38e5898ed9e74f0129de
Glenn Pinto <gvpinto@gmail.com>
2023-08-25 12:56:00 -0400

    239. Adding Fake Authentication: Implementing Login

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,24 +1,40 @@
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+
+// const FAKE_USER = {
+//     name: "Jack",
+//     email: "jack@example.com",
+//     password: "qwerty",
+//     avatar: "https://i.pravatar.cc/100?u=zz",
+// };
 
 function User() {
-  const user = FAKE_USER;
 
-  function handleClick() {}
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+    const { isAuthenticated } = useAuth();
 
-  return (
-    <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
-      <button onClick={handleClick}>Logout</button>
-    </div>
-  );
+    // const user = FAKE_USER;
+    const user = useAuth();
+
+    function handleClick() {
+        logout();
+        navigate('/');
+    }
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+    return (
+        <div className={styles.user}>
+            <img src={user.avatar} alt={user.name} />
+            <span>Welcome, {user.name}</span>
+            <button onClick={handleClick}>Logout</button>
+        </div>
+    );
 }
 
 export default User;
